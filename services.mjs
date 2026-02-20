ef71c7eb0bcab699052522e4c7dd4b7e299cfd3b
Vaddiraju Surya Teja <112264612+suryavaddiraju@users.noreply.github.com>
2026-02-20 01:53:39 +0000

    chore: auto release

--- services.mjs
+++ services.mjs
@@ -429,7 +429,7 @@ export const services = {
   "rds-signer": true,
   "s3-presigned-post": true,
   "s3-request-presigner": true,
-  "signature-v4-crt": false,
+  "signature-v4-crt": true,
   "signature-v4-multi-region": false,
   "signature-v4a": false,
   "smithy-client": false,
